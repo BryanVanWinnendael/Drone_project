951a2cc605356df055373ae8b357893d0beab050
Bryan Van Winnendael <bryan.vanwinnendael@student.ucll.be>
2023-02-15 16:41:10 +0100

    iets 2

--- main.py
+++ main.py
@@ -1,32 +1,89 @@
-from PyQt5 import QtWidgets, QtCore, QtGui
-import sys
-import win32gui
-from cloudPointWidget import CloudPointWidget
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+from pointCloudScreen import PointCloud_MainWindow
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
+from PyQt5.QtCore import QUrl
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        self.central_widget = QtWidgets.QStackedWidget()
-        self.setCentralWidget(self.central_widget)
-        nav_cloud_button = navCloudPointWidget(self)
-        nav_cloud_button.button.clicked.connect(self.getCloudPointWidget)
-        self.central_widget.addWidget(nav_cloud_button)
+    def __init__(self):
+        super().__init__()
+        central = QtWidgets.QWidget()
+        layout = QtWidgets.QVBoxLayout(central)
 
-    def getCloudPointWidget(self):
-        cloud_point_widget = CloudPointWidget(self)
-        self.central_widget.addWidget(cloud_point_widget)
-        self.central_widget.setCurrentWidget(cloud_point_widget)
+        self.testButton = QtWidgets.QPushButton('Upload file')
+        self.testButton.setObjectName("uploadbtn")
+        layout.addWidget(self.testButton)
 
-class navCloudPointWidget(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super(navCloudPointWidget, self).__init__(parent)
-        layout = QtWidgets.QHBoxLayout()
-        self.button = QtWidgets.QPushButton('Go to CloudPointWidget')
-        layout.addWidget(self.button)
-        self.setLayout(layout)
+        self.label = QtWidgets.QLabel()
+        self.label.setObjectName("label-error")
+        layout.addWidget(self.label)
+
+        self.setCentralWidget(central)
+        self.setAcceptDrops(True)
+        self.testButton.clicked.connect(self.openFileNameDialog)
+
+
+    def doRequest(self):   
+        url = "https://names.drycodes.com/10"
+        req = QNetworkRequest(QUrl(url))
+        
+        self.nam = QNetworkAccessManager()
+        self.nam.finished.connect(self.handleResponse)
+        self.nam.get(req)
+
+    def handleResponse(self, reply):
+        er = reply.error()
+        if er == QNetworkReply.NoError:
+            bytes_string = reply.readAll()
+            print(str(bytes_string, 'utf-8'))
+        else:
+            print("Error occured: ", er)
+            print(reply.errorString())
+       
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","Polygon Files (*.ply)", options=options)
+        if fileName and fileName.endswith('.ply'):
+            self.launchWindow(fileName)
+
+    def launchWindow(self, fileName=None):
+        self.test = PointCloud_MainWindow(fileName)
+        self.test.resize(800, 600)
+        self.test.backSignal.connect(self.show)
+        self.hide()
+        self.test.show()
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        files = [u.toLocalFile() for u in event.mimeData().urls()]
+        if (files[0].endswith('.ply') == True):
+            print("Opening ply file")
+            self.label.clear()
+            self.launchWindow(files[0])
+        else:
+            self.label.setText("Not a ply file")
+            print("Not a ply file")
+
+
+def GUI_Window():
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+
+    css="style.css"
+    with open(css,"r") as fh:
+        app.setStyleSheet(fh.read())
+
+    mainWindow = MainWindow()
+    mainWindow.resize(800, 600)
+    mainWindow.show()
+    sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication([])
-    window = MainWindow()
-    window.resize(800, 600)
-    window.show()
-    app.exec_()
+    GUI_Window()
--- pointCloudScreen.py
+++ pointCloudScreen.py
@@ -1,29 +1,33 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 import open3d as o3d
-from PyQt5 import QtCore
-from PyQt5 import QtGui
 import win32gui
 
-class CloudPointWidget(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.init_gui()
-        
-    def init_gui(self):
+class PointCloud_MainWindow(QtWidgets.QWidget):
+    backSignal = QtCore.pyqtSignal()
+    def __init__(self, fileName=None):
+        super().__init__()
         widget = QtWidgets.QWidget()
         layout = QtWidgets.QGridLayout(widget)
 
-        ply_point_cloud = o3d.data.PLYPointCloud()
-        pcd = o3d.io.read_point_cloud(ply_point_cloud.path)
+        print(fileName)
+
+        pcd = o3d.io.read_point_cloud(fileName)
         self.vis = o3d.visualization.Visualizer()
         self.vis.create_window()
         self.vis.add_geometry(pcd)
 
         hwnd = win32gui.FindWindowEx(0, 0, None, "Open3D")
+
         self.window = QtGui.QWindow.fromWinId(hwnd)    
         self.windowcontainer = self.createWindowContainer(self.window, widget)
-        layout.addWidget(self.windowcontainer, 0, 0)
+
+        button = QtWidgets.QPushButton('Upload new file')
+        button.clicked.connect(self.goBack)
+        layout.addWidget(button, 0, 0)
+        layout.addWidget(self.windowcontainer, 1, 0)
+
         self.setLayout(layout)
+
         timer = QtCore.QTimer(self)
         timer.timeout.connect(self.update_vis)
         timer.start(1)
@@ -31,3 +35,7 @@ class CloudPointWidget(QtWidgets.QWidget):
     def update_vis(self):
         self.vis.poll_events()
         self.vis.update_renderer()
+    
+    def goBack(self):
+        # self.close()
+        self.backSignal.emit()
