2f8a06250a963abc67e0592f0b74db5777f3b3f6
Bryan Van Winnendael <bryan.vanwinnendael@student.ucll.be>
2023-02-17 16:28:55 +0100

    begin styling

--- widgets/components/resultTable.py
+++ widgets/components/resultTable.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QHeaderView
 from PyQt5.QtGui import QIcon
 import csv
 from widgets.components.buttonTable import ButtonTable
@@ -9,10 +9,18 @@ class ResultTable(QTableWidget):
         file = open("data/results/output.csv", "r")
         self.data = list(csv.DictReader(file, delimiter=","))
         file.close()
-
+        
         super(ResultTable, self).__init__(len(self.data), len(self.data[0]) + 1)
         #QTableWidget.__init__(self, 4 , 3)
         
+        self.verticalHeader().setVisible(False)
+        self.setObjectName("resultTable")
+        
+        #header = header = self.horizontalHeader() 
+        #for i in range(len(self.data[0])):
+        #    header.setSectionResizeMode(i,  QHeaderView.ResizeMode.Stretch)
+        #header.setSectionResizeMode(len(self.data[0]) + 1, QHeaderView.ResizeMode.ResizeToContents)
+ 
         self.setData()
         self.resizeColumnsToContents()
         self.resizeRowsToContents()
