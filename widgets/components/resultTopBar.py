d50b8ddf0c0b336b7cf311d6b1da112df0fd714e
Bryan Van Winnendael <bryan.vanwinnendael@student.ucll.be>
2023-02-20 13:34:57 +0100

    export button

--- widgets/components/resultTopBar.py
+++ widgets/components/resultTopBar.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
 
 class ResultTopBar(QtWidgets.QWidget):
     def __init__(self, fileName, parent):
@@ -23,4 +24,26 @@ class ResultTopBar(QtWidgets.QWidget):
         self.layout.addWidget(self.backButton)
         self.layout.addWidget(self.nameLabel)
 
+        self.saveButton = QtWidgets.QPushButton("Export")
+        self.saveButton.setObjectName("exportbtn")
+        self.saveButton.setMinimumHeight(35)
+        self.saveButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.saveButton.clicked.connect(self.saveFileDialog)
+        self.layout.addWidget(self.saveButton)
+
         self.setLayout(self.layout)
+
+    
+    def saveFileDialog(self):
+        try:
+            filePath, _ = QFileDialog.getSaveFileName(self, "Save file", "",
+                            "CSV(*.csv);;")
+            result_path = "data/results/output.csv"
+
+            with open(result_path, "r") as infile:
+                data = infile.read()
+
+            with open(filePath, "w") as outfile:
+                outfile.write(data)
+        except:
+            print("No file selected")
