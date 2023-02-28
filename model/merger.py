183f1f38e049acb666843b538b1176173606bd87
Robbe Van de Velde <robbe.vandevelde@student.ucll.be>
2023-02-28 14:38:24 +0100

    Ids are now correctly updated

--- model/merger.py
+++ model/merger.py
@@ -12,7 +12,10 @@ class Merger():
         if len(ids) < 2:
             print("Select at least 2 segments to merge")
         else:
+            # Create new point cloud
             new_pcd = o3d.geometry.PointCloud()
+            
+            # Add points from all selected segments to new point cloud
             points = []
             for seg_id in ids:
                 if os.path.isfile(f"data/planes/plane_{seg_id}.ply"):
@@ -20,20 +23,24 @@ class Merger():
                     for point in np.asarray(pcd.points):
                         points.append(point)
 
+                    # Delete the segments
                     self.deleteSegment(seg_id)
 
             new_pcd.points = o3d.utility.Vector3dVector(points)
 
+            # Calculate new surface
             surface_area = ConvexHull(points, qhull_options='QJ').area / 2
 
             color = list(np.random.choice(range(256), size=3))
             new_pcd.paint_uniform_color([color[0] / 255, color[1] / 255, color[2] / 255])
             self.saveSegment(new_pcd, surface_area, color)
-            self.constructNewClassifiedPointCloud()
 
+            # Update the classified point cloud
+            self.constructNewClassifiedPointCloud()
             self.parent.classifiedResultChanged()
-            self.parent.dataChanged()
-            # self.reassignSegmentIds()
+
+            # Update the ids
+            self.reassignSegmentIds()
 
     def saveSegment(self, pcd, surface_area, rgb):  
         print("saving...")      
@@ -79,14 +86,23 @@ class Merger():
 
     def reassignSegmentIds(self):
         with open("data/results/output.csv", "r") as f:
-            lines = f.readlines()
+            lines = f.readlines()[1:]
 
-            for i, line in enumerate(lines):
-                # Rename plane to new id
+            # Temporarily rename all files
+            for line in lines:
                 print(f"data/planes/plane_{line.split(',')[0]}.ply")
-                os.rename(f"data/planes/plane_{line.split(',')[0]}.ply", f"data/planes/plane_{i + 1}.ply")
+                os.rename(f"data/planes/plane_{line.split(',')[0]}.ply", f"data/planes/plane_{line.split(',')[0]}_temp.ply")
+            
+            # Rename plane to new id
+            for i, line in enumerate(lines):
+                os.rename(f"data/planes/plane_{line.split(',')[0]}_temp.ply", f"data/planes/plane_{i + 1}.ply")
 
             f.close()
+
+        # Update CSV
+        df = pd.read_csv("data/results/output.csv")
+        df["Segment"] = range(1, len(df) + 1)
+        df.to_csv("data/results/output.csv", index=False)
 
         
         
